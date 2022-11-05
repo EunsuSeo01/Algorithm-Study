efddeff53a4b0db91d525744caae63d4fbe0037f
EunsuSeo01 <eunsu0613@naver.com>
2022-11-06 06:28:03 +0900

    Baekjoon - 기본 수학1 : 2839번 풀이 수정(5의 곱을 기준으로)

--- Baekjoon/2839.py
+++ Baekjoon/2839.py
@@ -3,14 +3,13 @@ answer = []
 three = 0
 five = 0
 while True:
-    five = (N - 3 * three) / 5
-    if (N - 3 * three) % 5 == 0:
-        answer.append(int(three + five))
-    if five <= 0:
+    three = (N - 5 * five) / 3
+    if three < 0:
         break
-    else:
-        three += 1
+    if (N - 5 * five) % 3 == 0:
+        answer.append(three + five)
+    five += 1
 if len(answer) == 0:
     print(-1)
 else:
-    print(min(answer))
+    print(int(min(answer)))

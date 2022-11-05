6a64b902fff19fde42d5db89aaad4f7e8e53bdb1
EunsuSeo01 <eunsu0613@naver.com>
2022-11-06 00:18:28 +0900

    Baekjoon - 기본 수학1 : 2839번 변수 이름 수정

--- Baekjoon/2839.py
+++ Baekjoon/2839.py
@@ -1,18 +1,16 @@
 N = int(input())
 answer = []
-a = 0
-b = 0
-c = 0
+three = 0
+five = 0
 while True:
-    b = (N - 3 * a) % 5
-    c = (N - 3 * a) / 5
-    if b == 0:
-        answer.append(a + c)
-    a += 1
-    if c <= 0:
+    five = (N - 3 * three) / 5
+    if (N - 3 * three) % 5 == 0:
+        answer.append(int(three + five))
+    if five <= 0:
         break
+    else:
+        three += 1
 if len(answer) == 0:
     print(-1)
 else:
-    print(int(min(answer)))
-
+    print(min(answer))

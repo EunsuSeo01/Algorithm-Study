8333471b9f4e045639ec89ce5ae1d14d4e9457a9
EunSuSeo01 <eunsu0613@naver.com>
2022-12-04 18:45:22 +0900

    Baekjoon - 정렬 : 1181번 풀이 개선

--- Baekjoon/정렬/1181.py
+++ Baekjoon/정렬/1181.py
@@ -2,8 +2,14 @@
 import sys
 
 N = int(sys.stdin.readline().strip())
-words = set([sys.stdin.readline().strip() for i in range(N)])
-lengths = [[len(word), word] for word in words]
-lengths.sort()
-for _, w in lengths:
-    print(w)
+# 입력을 N개를 받아서 집합으로 만든다 -> 중복된 단어 제거
+words = set(sys.stdin.readline().strip() for i in range(N))
+# 정렬 함수를 쓰기 위해 list로 형변환을 한다
+words = list(words)
+# words의 단어들을 사전 순으로 정렬한다
+words.sort()
+# 사전 순으로 정렬된 단어들을 길이 순으로 정렬한다 -> 같은 길이의 단어는 사전 순이 유지된다
+words.sort(key=len)
+for word in words:
+    print(word)
+

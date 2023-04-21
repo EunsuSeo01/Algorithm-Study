058c06b1a6f179404e9b2d1ce908e32e40a00aac
EunsuSeo01 <eunsu0613@naver.com>
2023-04-22 00:51:26 +0900

    Baekjoon - 1284번 : 집 주소 풀이 개선

--- Baekjoon/수학/1284. 집 주소/집 주소.py
+++ Baekjoon/수학/1284. 집 주소/집 주소.py
@@ -3,11 +3,11 @@ import sys
 while True:
     # 경계와 숫자 사이의 간격(좌우 총 2cm)으로 초기값을 설정
     width = 2
-    nums = list(sys.stdin.readline().strip())
+    nums = sys.stdin.readline().strip()
     # 각 숫자 사이의 1cm 여백 추가
     width += len(nums) - 1
     # 0이면 종료
-    if nums[0] == '0':
+    if nums == '0':
         break
     # 각 자리를 확인하면서 해당하는 cm만큼 추가
     for n in nums:

33610eb471dcef50ef38172f19a8b9ef74c1d033
EUNSU SEO <eunsu0613@naver.com>
2023-05-11 00:13:29 +0900

    [Bronze I] Title: 슈퍼 마리오, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- Baekjoon/구현/2851. 슈퍼 마리오/슈퍼 마리오.py
+++ Baekjoon/구현/2851. 슈퍼 마리오/슈퍼 마리오.py
@@ -3,19 +3,14 @@ import sys
 input_list = [int(sys.stdin.readline().strip()) for _ in range(10)]
 
 score = 0
-two = []
 for i in input_list:
     score += i
-    if score >= 100:
-        two.append(score)
-        two.append(score - i)
+    if score == 100:
         break
-if len(two) == 0:
-    print(score)
-else:
-    if abs(100 - two[0]) > abs(100 - two[1]):
-        print(two[1])
-    elif abs(100 - two[0]) < abs(100 - two[1]):
-        print(two[0])
-    else:
-        print(max(two))
+    elif score > 100:
+        if score - 100 > 100 - (score - i):
+            score -= i
+        else:
+            break
+        break
+print(score)

281add8f3f65dc60693d8f3f5531beb1eea46170
EUNSU SEO <eunsu0613@naver.com>
2023-09-04 23:51:22 +0900

    [Silver IV] Title: 덱, Time: 28 ms, Memory: 69108 KB -BaekjoonHub

--- Baekjoon/구현/10866. 덱/덱.swift
+++ Baekjoon/구현/10866. 덱/덱.swift
@@ -1,36 +1,25 @@
-let num = readLine().map { Int($0) }
-var arr = [Int]()
-for _ in 1...num!! {
+let N = readLine().map { Int($0) }
+var deque = [Int]()
+
+for _ in 1...N!! {
     let input = readLine()!.split(separator: " ")
     switch input[0] {
     case "push_back":
-        arr.append(Int(input[1])!)
+        deque.append(Int(input[1])!)
     case "push_front":
-        arr = [Int(input[1])!] + arr
+        deque.insert(Int(input[1])!, at: 0)
     case "front":
-        print(arr.isEmpty ? -1 : arr[0])
+        print(deque.isEmpty ? -1 : deque[0])
     case "back":
-        print(arr.isEmpty ? -1 : arr[arr.count - 1])
+        print(deque.isEmpty ? -1 : deque[deque.count - 1])
     case "size":
-        print(arr.count)
+        print(deque.count)
     case "empty":
-        print(arr.isEmpty ? 1 : 0)
+        print(deque.isEmpty ? 1 : 0)
     case "pop_front":
-        if arr.isEmpty {
-            print(-1)
-        } else {
-            let firstIndex = 0
-            print(arr[firstIndex])
-            arr.remove(at: firstIndex)
-        }
+        print(deque.isEmpty ? -1 : deque.removeFirst())
     case "pop_back":
-        if arr.isEmpty {
-            print(-1)
-        } else {
-            let lastIndex = arr.count - 1
-            print(arr[lastIndex])
-            arr.remove(at: lastIndex)
-        }
+        print(deque.isEmpty ? -1 : deque.removeLast())
     default:
         print()
     }

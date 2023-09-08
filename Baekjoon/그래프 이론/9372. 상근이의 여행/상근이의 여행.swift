b44ad7efbc6e26d9f84714681a972c567128cabd
EUNSU SEO <eunsu0613@naver.com>
2023-09-08 23:23:50 +0900

    [Silver IV] Title: 상근이의 여행, Time: 148 ms, Memory: 69104 KB -BaekjoonHub

---
+++ Baekjoon/그래프 이론/9372. 상근이의 여행/상근이의 여행.swift
@@ -0,0 +1,13 @@
+let T = Int(readLine()!)!
+
+for _ in 0..<T {
+    let info = readLine()!.split(separator: " ")
+    let N = Int(info[0])!
+    let M = Int(info[1])!
+    
+    for _ in 0..<M {
+        let abInfo = readLine()!.split(separator: " ")
+    }
+    
+    print(N-1) // 무슨... 문제가 이래?
+}

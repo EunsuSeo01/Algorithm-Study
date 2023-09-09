e64f4f3b8ed7ee3d0a3d2e8deaa2c78247686a3b
EUNSU SEO <eunsu0613@naver.com>
2023-09-09 23:31:24 +0900

    [Silver V] Title: 번데기, Time: 12 ms, Memory: 79508 KB -BaekjoonHub

--- Baekjoon/구현/15721. 번데기/번데기.swift
+++ Baekjoon/구현/15721. 번데기/번데기.swift
@@ -11,64 +11,28 @@ for _ in 0..<A {
 
 var times = 2
 var index = 0
+var count = 0
 var num = 1
 
-if signal == 0 {
-    while true {
-        if num == T {
-            print(index % A)
-            break
-        }
-        members[index%A] = num
-        if num == T {
-            print(index % A)
-            break
-        }
-        num += 1
-        members[(index+2)%A] = num
-        if num == T {
-            print((index+2)%A)
-            break
-        }
-        num += 1
-        index += 4
-        for _ in 0..<times {
-            members[index%A] = num
-            if num == T {
-                print(index % A)
-                exit(0)
-            }
-            num += 1
-            index += 1
-        }
-        index += times
-        times += 1
+while true {
+    var arr = [0,1,0,1]
+    for _ in 0..<times {
+        arr.append(0)
     }
-} else {
-    while true {
-        members[(index+1)%A] = num
-        if num == T {
-            print((index+1) % A)
-            break
-        }
-        num += 1
-        members[(index+3)%A] = num
-        if num == T {
-            print((index+3)%A)
-            break
-        }
-        num += 1
-        index += 4
-        index += times
-        for _ in 0..<times {
-            members[index%A] = num
-            if num == T {
-                print(index % A)
-                exit(0)
-            }
-            num += 1
-            index += 1
-        }
-        times += 1
+    for _ in 0..<times {
+        arr.append(1)
     }
+    
+    for n in arr {
+        if n == signal {
+            count += 1
+        }
+        if count == T {
+            print(index)
+            exit(0)
+        }
+        index += 1
+        index %= A
+    }
+    times += 1
 }

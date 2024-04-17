c6d28fb85874c1718866f1596aa9e111859963c2
EUNSU SEO <eunsu0613@naver.com>
2024-04-17 20:35:07 +0900

    [Bronze I] Title: 소인수분해, Time: 56 ms, Memory: 79508 KB -BaekjoonHub

---
+++ Baekjoon/수학/11653. 소인수분해/소인수분해.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+var N = Int(readLine()!)!
+
+while N != 1 {
+    for i in 2...N {
+        if N % i == 0 {
+            print(i)
+            N = Int(N / i)
+            break
+        }
+    }
+}
